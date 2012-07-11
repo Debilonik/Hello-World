b867edbae935983d392d532cd8a381d9d96ce018
Alexey Gorovoy <debilonik@gmail.com>
2012-07-11 20:30:10 +0300

    Some Days Later Commit

--- main.cpp
+++ main.cpp
@@ -4,4 +4,6 @@ using namespace std;
 int main (){
 	cout << "Hello Git!" << endl;
 	cout << "Second Commit!" << endl; //commit 2nd
+	cout << "Some Days Later I've commited"
+	<< " these two lines of code..." << endl;
 }
